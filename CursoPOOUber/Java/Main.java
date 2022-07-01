6ec3995412b7f54823b6e09c23a897ae47aa9356
juxxsrck <justine.pulido@gmail.com>
2022-06-30 19:11:02 -0500

    Objetos. Dando vida a nuestras clases en Java y Python

--- CursoPOOUber/Java/Main.java
+++ CursoPOOUber/Java/Main.java
@@ -1,5 +1,16 @@
 class Main {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        Car car = new Car();
+        car.license = "JSP123";
+        car.driver = "Stiven Pulido";
+        car.passenger = 4;
+        car.printDataCar();
+
+        Car car2 = new Car();
+        car2.license = "QNH654";
+        car2.driver = "Andres Ovalle";
+        car2.passenger = 3;
+        car2.printDataCar();
     }
 }
--- CursoPOOUber/Python/main.py
+++ CursoPOOUber/Python/main.py
@@ -1,2 +1,13 @@
+import imp
+from car import Car
 if __name__ == "__main__":
     print('Hola Mundo')
+    car = Car()
+    car.license = "GAP567"
+    car.driver = "German Pulido"
+    print(vars(car))
+
+    car2 = Car()
+    car2.license = 'MBM098'
+    car2.driver = 'Melany Barreto'
+    print(vars(car2))
