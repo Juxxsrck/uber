d36ca4f2b001beb12cead68ab0d9d10c4322e01b
juxxsrck <justine.pulido@gmail.com>
2022-06-30 20:41:57 -0500

    Declarando un método Constructor en Java y JavaScript

---
+++ CursoPOOUber/JS/index.js
@@ -0,0 +1,3 @@
+var car = new Car("JSP123", new Account("Stiven Pulido", "1032098765"))
+car.passenger = 4;
+car.printDataCar();
--- CursoPOOUber/Java/Main.java
+++ CursoPOOUber/Java/Main.java
@@ -1,15 +1,11 @@
 class Main {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
-        Car car = new Car();
-        car.license = "JSP123";
-        car.driver = "Stiven Pulido";
+        Car car = new Car("JSP123", new Account("Stiven Pulido", "1014652939"));
         car.passenger = 4;
         car.printDataCar();
 
-        Car car2 = new Car();
-        car2.license = "QNH654";
-        car2.driver = "Andres Ovalle";
+        Car car2 = new Car("QNH654", new Account("Andres Ovalle", "1022345678"));
         car2.passenger = 3;
         car2.printDataCar();
     }
