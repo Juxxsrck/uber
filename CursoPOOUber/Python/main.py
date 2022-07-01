1bbf7f23c9987854b493faa49982801a2852797e
juxxsrck <justine.pulido@gmail.com>
2022-06-30 21:18:36 -0500

    Declarando un metodo constructor en Python

--- CursoPOOUber/Python/main.py
+++ CursoPOOUber/Python/main.py
@@ -1,13 +1,9 @@
-import imp
 from car import Car
+from account import Account
+
 if __name__ == "__main__":
     print('Hola Mundo')
-    car = Car()
-    car.license = "GAP567"
-    car.driver = "German Pulido"
+    
+    car = Car("JSP123", Account("Stiven Pulido", "1014098765"))
     print(vars(car))
-
-    car2 = Car()
-    car2.license = 'MBM098'
-    car2.driver = 'Melany Barreto'
-    print(vars(car2))
+    print(vars(car.driver))
