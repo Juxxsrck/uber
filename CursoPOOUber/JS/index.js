31743d95786cf09a3761dd38d92e1739743c8e20
juxxsrck <justine.pulido@gmail.com>
2022-07-01 22:08:18 -0500

    Jerarquia de Account que se encontraba pendiente

--- CursoPOOUber/JS/index.js
+++ CursoPOOUber/JS/index.js
@@ -1,3 +1,7 @@
 var car = new Car("JSP123", new Account("Stiven Pulido", "1032098765"))
 car.passenger = 4;
 car.printDataCar();
+
+var uberX = new UberX("JSP123", new Account("Stiven Pulido", "1014987432"), "Renault", "Twingo")
+uberX.passenger = 4;
+uberX.printDataCar();
---
+++ CursoPOOUber/Python/Driver.py
@@ -0,0 +1,6 @@
+from msilib.schema import Class
+from account import Account
+
+class Driver(Account):
+    def __init__(self, name, document, email, password):
+        super().__init__(name, document, email, password)
