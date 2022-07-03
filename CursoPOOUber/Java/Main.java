b81d519add265aec9dd64ba84ad74f76e0d79192
juxxsrck <justine.pulido@gmail.com>
2022-07-02 19:30:46 -0500

    Encapsulando atributos en Java

--- CursoPOOUber/Java/Main.java
+++ CursoPOOUber/Java/Main.java
@@ -1,12 +1,13 @@
 class Main {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
-        Car car = new Car("JSP123", new Account("Stiven Pulido", "1014652939"));
-        car.passenger = 4;
-        car.printDataCar();
+        UberX uberX = new UberX("JSP123", new Account("Stiven Pulido", "1014652939", "stiven.pulido@correo.com", "123456")
+        ,"Renault", "Twingo");
+        uberX.setPassenger(3);
+        uberX.printDataCar();
 
-        Car car2 = new Car("QNH654", new Account("Andres Ovalle", "1022345678"));
-        car2.passenger = 3;
-        car2.printDataCar();
+        // Car car2 = new Car("QNH654", new Account("Andres Ovalle", "1022345678", "andres.ovalle@correo.com", "098765"));
+        // car2.passenger = 3;
+        // car2.printDataCar();
     }
 }
--- CursoPOOUber/Java/UberPool.java
+++ CursoPOOUber/Java/UberPool.java
@@ -2,7 +2,7 @@ class UberPool extends Car {
     String brand;
     String model;
     
-    public UberX(String license, Account driver, String brand, String model){
+    public UberPool(String license, Account driver, String brand, String model){
         super(license, driver);
         this.brand = brand;
         this.model = model;
